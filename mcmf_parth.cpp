b5a6f220ea2e79cc5379f97932c5f5e87eca5a43
parthmittal <parth15069@iiitd.ac.in>
2017-11-05 16:54:19 +0530

    added my implementation of MCMF

---
+++ mcmf_parth.cpp
@@ -0,0 +1,134 @@
+#include <vector>
+#include <functional>
+#include <queue>
+
+using namespace std;
+
+typedef long double ld;
+typedef long long ll;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+
+struct edge_t {
+	int u, v;
+	ll cap, cost;
+	int id; /* use if you want to recover the edges */
+	edge_t (int from, int to, ll cap, ll cost, int id = -1)
+		:u(from), v(to), cap(cap), cost(cost), id(id) {}
+};
+
+const ll inf = 5e18 + 420;
+struct mcmf_t {
+	vector<edge_t> edges;
+	vector< vector<int> > adj; /* adj[i] <- indices of edges incident on i */
+
+	vector<ll> pot;
+
+	mcmf(int N)
+		: adj(N) {}
+	void add_edge(int u, int v, ll cap, ll cost, int id = -1)
+	{
+		adj[u].push_back(edges.size());
+		edges.push_back({u, v, cap, cost, id});
+		adj[v].push_back(edges.size());
+		edges.push_back({v, u, 0, -cost, id});
+	}
+
+	vector<ll> BellmanFord(int source)
+	{
+		int N = adj.size();
+		vector<ll> dist(N, inf);
+		dist[source] = 0;
+
+		for (int iter = 0; iter < N; ++iter) {
+			for (auto &e : edges) {
+				if (e.cap > 0 && dist[e.v] > dist[e.u] + e.cost) {
+					dist[e.v] = dist[e.u] + e.cost;
+				}
+			}
+		}
+
+		return dist;
+	}
+
+	/* call this once, it return
+	 * < min(max_flow, target_flow), cost >
+	 * runtime: O(VE + FE*log(E)),
+	 * where F is max_flow */
+	pair<ll, ll> min_cost_max_flow(int source, int sink, ll target_flow = inf)
+	{
+		pot = BellmanFord(source);
+		int N = adj.size();
+		typedef pair<ll, int> pqnode_t;
+
+		ll flow = 0;
+		ll cost = 0;
+		while(target_flow - flow > 0) {
+			vector<ll> d(N, inf);
+			vector<int> edge_index(N, -1);
+			vector<int> popped(N);
+			d[source] = 0;
+
+			priority_queue< pqnode_t, vector<pqnode_t>, greater<pqnode_t> > dfq;
+			dfq.push({0, source});
+
+			while(!dfq.empty()) {
+				int u = dfq.top().second;
+				ll w  = dfq.top().first;
+				dfq.pop();
+				if (popped[u]) {
+					continue;
+				}
+				popped[u] = 1;
+				for (auto &eidx : adj[u]) {
+					const auto &e = edges[eidx];
+					if (e.cap > 0) {
+						ll cand = d[e.u] + (e.cost - pot[e.v] + pot[e.u]);
+						if (cand < d[e.v]) {
+							d[e.v]          = cand;
+							edge_index[e.v] = eidx;
+							dfq.push({d[e.v], e.v});
+						}
+					}
+				}
+			}
+
+			if (edge_index[sink] == -1) {
+				return {flow, cost};
+			} else {
+				/* update the potentials */
+				for (int u = 0; u < N; ++u) {
+					if (edge_index[u] != -1) {
+						pot[u] += d[u];
+					}
+				}
+				/* first compute the amount of flow we can send */
+				ll aug_flow = target_flow - flow;
+				for (int u = sink; u != source; ) {
+					const auto &e = edges[edge_index[u]];
+					aug_flow = min(aug_flow, e.cap);
+
+					u = e.u;
+				}
+
+				/* now update total flow */
+				flow += aug_flow;
+
+				/* now update graph edges and also add in cost of aug_flow */
+				for (int u = sink; u != source; ) {
+					edge_t &fwd = edges[edge_index[u]];
+					edge_t &rev = edges[edge_index[u] ^ 1];
+					rev.cap += aug_flow;
+					fwd.cap -= aug_flow;
+
+					cost += aug_flow * fwd.cost;
+
+					u = fwd.u;
+				}
+			}
+
+		}
+
+		return {flow, cost};
+	}
+};
